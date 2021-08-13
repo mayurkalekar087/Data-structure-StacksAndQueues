d8420b0d760c09eff72a053454f96efd61ccf3cf
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-13 16:31:12 +0530

    feat:Welcome to Stacks and Queues Program

---
+++ StacksAndQueues/LinkedListStacks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    public class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stacked ", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+       
+    }
+
+}
+
+    
---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stacks and Queues Program!");
+
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            linkedListStack.Display();
+        }
+    }
+}
