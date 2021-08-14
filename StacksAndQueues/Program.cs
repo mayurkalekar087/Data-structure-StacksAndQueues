6c40dc307d288fdc08ec07cb96797e0611f4110d
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-14 12:34:19 +0530

    feat:Ability to create Queue

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -8,15 +8,22 @@ namespace StacksAndQueues
         {
             Console.WriteLine("Welcome to stacks and Queues Program !");
 
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.push(70);
-            linkedListStack.push(30);
-            linkedListStack.push(56);
-            linkedListStack.pop();
-            linkedListStack.peek();
-            linkedListStack.isEmpty();
-            linkedListStack.isFull();
-            linkedListStack.Display();
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.push(70);
+            //linkedListStack.push(30);
+            //linkedListStack.push(56);
+            //linkedListStack.pop();
+            //linkedListStack.peek();
+            //linkedListStack.isEmpty();
+            //linkedListStack.isFull();
+            //linkedListStack.Display();
+            LinkedListQueues linkedListQueues = new LinkedListQueues();
+            linkedListQueues.Enqueue(56);
+            linkedListQueues.Enqueue(30);
+            linkedListQueues.Enqueue(70);
+            linkedListQueues.Display();
+            Console.WriteLine("------------------");
+           
         }
     }
 }
--- StacksAndQueues/Queues.cs
+++ StacksAndQueues/Queues.cs
@@ -4,7 +4,50 @@ using System.Text;
 
 namespace StacksAndQueues
 {
-    class Queues
+    public class LinkedListQueues
     {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        //internal Node Dequeue()
+        //{
+        //    if (this.head == null)
+        //    {
+        //        return null;
+        //    }
+        //    this.head = this.head.next;
+        //    return this.head;
+        //}
+
     }
 }
